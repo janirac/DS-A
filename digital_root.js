9220aad821eeda7d12996dccea97307af430d6ac
Janira Crispin <janiracrispin@gmail.com>
2023-03-22 00:40:51 -0400

    first commit

---
+++ digital_root.js
@@ -0,0 +1,8 @@
+// Write a method, digital_root(num). It should sum the digits of a positive integer. 
+// If it is greater than or equal to 10, sum the digits of the resulting number. 
+// Keep repeating until there is only one digit in the result, called the "digital root". 
+// Do not use string conversion within your method.
+
+// Slowed down, think a little more. Spend more time understanding the problem and asking questions
+// Talk about your understanding of the problem and reitterate the question
+// Base case scenerio 
---
+++ two_sum.rb
@@ -0,0 +1,19 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+def two_sum(nums, target)
+    indices = []
+    nums.each_with_index do |num1, index1|
+        nums.each_with_index do |num2, index2|
+            if index2 > index1 && num1 + num2 == target 
+                indices << index1
+                indices << index2
+            end 
+        end 
+    end 
+
+    indices
+end
