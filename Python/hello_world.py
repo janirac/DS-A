b95cf8d4e551a16e0f2a40d95c2dbb337bf53156
Janira Crispin <janiracrispin@gmail.com>
2023-07-07 14:16:55 -0400

    Python conditions and arithmetic

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -24,8 +24,29 @@
 
 # print(age)
 
-course = "Python for Beginners"
-new_course = course.isdigit()
-print('Python' in course) #returns a boolean if the given str in found where as using the find will return the index
-print(new_course)
-print(course)
+# course = "Python for Beginners"
+# new_course = course.isdigit()
+# print('Python' in course) #returns a boolean if the given str in found where as using the find will return the index
+# print(new_course)
+# print(course)
+
+# x = (10 + 3) * 2
+# x = 3 == 3
+# print(x)
+
+# price = 5
+
+# print( not price > 10)
+
+temperature = 5
+
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else: 
+    print("It's cold")
+print("Done")
