32be1c6a7f924b92e577343823415057cd07d3a8
Janira Crispin <janiracrispin@gmail.com>
2023-07-07 11:24:29 -0400

    Python Fundamentals Beginners Practice

---
+++ Python/hello_world.py
@@ -0,0 +1,19 @@
+# print('Hello World') #can write a string like this "" or ''
+
+# age = 20
+# age = 30
+# age = "I am a string now"
+# age = False
+
+
+# first_name = "John"
+# last_name = "Smith"
+# age = 20
+# new_patient = True
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+
+# print(age)
